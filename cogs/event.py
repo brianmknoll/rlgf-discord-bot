1db3bec1b91e22392bd937b7a920d1bc59701baa
Ben Rooke <ben_rooke@icloud.com>
2025-04-29 16:31:46 -0700

    Add a command to create an event via our API

--- cogs/event.py
+++ cogs/event.py
@@ -1,5 +1,10 @@
+import requests
+
 from discord.ext import commands
-import discord
+
+
+SERVER_URL='https://rlgf-service-821668527340.us-west1.run.app'
+EVENTS_URI=f'{SERVER_URL}/events'
 
 
 class EventCog(commands.Cog):
@@ -27,6 +32,21 @@ class EventCog(commands.Cog):
         """
         await ctx.message.delete()
         await ctx.send(message)
+
+    @commands.command(name="event")
+    async def event(self, ctx, *, message):
+        if ctx.guild is None:
+            await ctx.send('This command can only be used in a server.')
+            return
+        r = requests.post(EVENTS_URI, json={
+            'guild_id': f'{ctx.guild.name}__{ctx.guild.id}',
+            'name': message
+        })
+        print(r.raise_for_status())
+        if r.status_code // 100 != 2:
+            await ctx.send(f'Error creating event: {r.text}')
+        else:
+            await ctx.send(f'Event "{message}" created in server {ctx.guild.name}.')
 
 
 async def setup(bot):
